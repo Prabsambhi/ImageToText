1d9c109eb5030b771ec3ccc9ec3f65f1fefbd9b4
Prabsambhi <prabsambhi@gmail.com>
2022-07-19 13:51:08 +0530

    news

--- lib/recognitionscreen.dart
+++ lib/recognitionscreen.dart
@@ -14,34 +14,7 @@ class RecognitionScreen extends StatefulWidget {
 }
 
 class _RecognitionScreenState extends State<RecognitionScreen> {
-  late File pickedImage;
-  optionsdialog(BuildContext context) {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return SimpleDialog(
-          children: [
-            SimpleDialogOption(
-                onPressed: () => pickimage(ImageSource.gallery),
-                child: Text("Gallery")),
-            SimpleDialogOption(
-                onPressed: () => pickimage(ImageSource.camera),
-                child: Text("Camera")),
-            SimpleDialogOption(
-                onPressed: () => Navigator.pop(context), child: Text("Cancel"))
-          ],
-        );
-      },
-    );
-  }
-
-  pickimage(ImageSource source) async {
-    final image = await ImagePicker().getImage(source: source);
-    setState(() {
-      pickedImage = File(image!.path);
-    });
-  }
-
+  String? imagePath;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +33,9 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
             FloatingActionButton(
               backgroundColor: Color(0xffEC360E),
               heroTag: null,
-              onPressed: () {},
+              onPressed: () {
+                pickmedia();
+              },
               child: Icon(Icons.reply, size: 34),
             )
           ],
@@ -81,16 +56,17 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                       ),
                     ),
                     SizedBox(height: 30),
-                    InkWell(
-                      onTap: () => optionsdialog(context),
-                      child: Image(
-                          width: 256,
-                          height: 256,
-                          fit: BoxFit.fill,
-                          image: pickedImage == null
-                              ? AssetImage('images/uploadfile.png')
-                              : FileImage(pickedImage) as ImageProvider),
-                    ),
+                    (imagePath != null)
+                        ? Image.file(File(imagePath!))
+                        : InkWell(
+                            onTap: () {},
+                            child: Image(
+                                width: 256,
+                                height: 256,
+                                fit: BoxFit.fill,
+                                image: AssetImage('images/uploadfile.png'))
+                            // : FileImage(pickedImage) as ImageProvider),
+                            ),
                     SizedBox(
                       height: 30,
                     ),
@@ -104,5 +80,13 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                     )
                   ],
                 ))));
+  }
+
+  void pickmedia() async {
+    XFile? file = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      imagePath = file.path;
+      setState(() {});
+    }
   }
 }
