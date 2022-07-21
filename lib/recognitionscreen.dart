595f28c031afb00e134824f71007fca808a1d84a
Prabsambhi <prabsambhi@gmail.com>
2022-07-22 01:23:45 +0530

    finall

--- lib/recognitionscreen.dart
+++ lib/recognitionscreen.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, import_of_legacy_library_into_null_safe, unnecessary_null_comparison
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, import_of_legacy_library_into_null_safe, unnecessary_null_comparison, prefer_interpolation_to_compose_strings
 
-// import 'dart:html';
 import 'dart:io';
-// import 'package:ocrapp/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
 
 class RecognitionScreen extends StatefulWidget {
@@ -14,7 +13,12 @@ class RecognitionScreen extends StatefulWidget {
 }
 
 class _RecognitionScreenState extends State<RecognitionScreen> {
-  String? imagePath;
+  // String? imagePath;
+  bool textScanning = false;
+
+  XFile? imageFile;
+
+  String scannedText = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +29,7 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
             FloatingActionButton(
               heroTag: null,
               onPressed: () {
-                pickmedia(ImageSource.camera);
+                getImage(ImageSource.camera);
               },
               child: Icon(Icons.copy, size: 28),
             ),
@@ -36,7 +40,7 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
               backgroundColor: Color(0xffEC360E),
               heroTag: null,
               onPressed: () {
-                pickmedia(ImageSource.gallery);
+                getImage(ImageSource.gallery);
               },
               child: Icon(Icons.reply, size: 34),
             )
@@ -58,20 +62,18 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                       ),
                     ),
                     SizedBox(height: 30),
-                    (imagePath != null)
-                        ? Image.file(File(imagePath!))
-                        : InkWell(
-                            onTap: () {},
-                            child: Image(
-                                width: 256,
-                                height: 256,
-                                fit: BoxFit.fill,
-                                image: AssetImage('images/uploadfile.png'))),
-                    SizedBox(
-                      height: 30,
-                    ),
+                    if (textScanning) const CircularProgressIndicator(),
+                    if (!textScanning && imageFile == null)
+                      InkWell(
+                          onTap: () {},
+                          child: Image(
+                              width: 256,
+                              height: 256,
+                              fit: BoxFit.fill,
+                              image: AssetImage('images/uploadfile.png'))),
+                    if (imageFile != null) Image.file(File(imageFile!.path)),
                     Text(
-                      "Lorem ipsum",
+                      scannedText,
                       style: TextStyle(
                         fontSize: 30,
                         color: Color(0xff1738EB),
@@ -82,15 +84,43 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                 ))));
   }
 
-  void pickmedia(ImageSource source) async {
-    XFile? file = await ImagePicker().pickImage(
-        source: source,
-        // maxWidth: 400,
-        // maxHeight: 256,
-        imageQuality: 100);
-    if (file != null) {
-      imagePath = file.path;
+  // void pickmedia(ImageSource source) async {
+  //   XFile? file = await ImagePicker().pickImage(
+  //       source: source, maxWidth: 256, maxHeight: 256, imageQuality: 100);
+  //   if (file != null) {
+  //     imagePath = file.path;
+  //     setState(() {});
+  //   }
+  // }
+  void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        textScanning = true;
+        imageFile = pickedImage;
+        setState(() {});
+        getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      textScanning = false;
+      imageFile = null;
+      scannedText = "Error occured while scanning";
       setState(() {});
     }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognisedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannedText = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = scannedText + line.text + "\n";
+      }
+    }
+    textScanning = false;
+    setState(() {});
   }
 }
