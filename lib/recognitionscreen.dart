28508be3008324f19288a64a74ef63879c394ad1
Prabsambhi <prabsambhi@gmail.com>
2023-06-24 16:02:36 +0530

    letest

--- lib/recognitionscreen.dart
+++ lib/recognitionscreen.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class RecognitionScreen extends StatefulWidget {
   const RecognitionScreen({Key? key}) : super(key: key);
@@ -14,40 +15,53 @@ class RecognitionScreen extends StatefulWidget {
 
 class _RecognitionScreenState extends State<RecognitionScreen> {
   // String? imagePath;
+  FlutterTts flutterTts = FlutterTts();
   bool textScanning = false;
 
   XFile? imageFile;
 
   String scannedText = "";
+  List<String> sentences = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xffF8F9FB),
-        floatingActionButton: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              heroTag: null,
-              onPressed: () {
-                getImage(ImageSource.camera);
-              },
-              child: Icon(Icons.copy, size: 28),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              backgroundColor: Color(0xffEC360E),
-              heroTag: null,
-              onPressed: () {
-                getImage(ImageSource.gallery);
-              },
-              child: Icon(Icons.reply, size: 34),
-            )
-          ],
+        backgroundColor: Color.fromRGBO(248, 249, 251, 1),
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.fromLTRB(0, 5, 0, 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  speakText(scannedText);
+                },
+                child: Icon(Icons.speaker, size: 28),
+              ),
+              FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  setState(() {
+                    imageFile = null;
+                    scannedText = "";
+                    sentences = [];
+                  });
+                },
+                child: Icon(Icons.clear, size: 28),
+              ),
+              FloatingActionButton(
+                heroTag: null,
+                onPressed: () {
+                  getImage(ImageSource.camera);
+                },
+                child: Icon(Icons.camera, size: 28),
+              ),
+            ],
+          ),
         ),
         body: SingleChildScrollView(
             child: Container(
+                padding: EdgeInsets.all(8),
                 alignment: Alignment.center,
                 child: Column(
                   children: [
@@ -57,27 +71,43 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
                       "Text Recognition",
                       style: TextStyle(
                         fontSize: 30,
-                        color: Color(0xff1738EB),
+                        color: Color.fromARGB(255, 103, 125, 255),
                         fontWeight: FontWeight.w800,
                       ),
                     ),
                     SizedBox(height: 30),
                     if (textScanning) const CircularProgressIndicator(),
                     if (!textScanning && imageFile == null)
-                      InkWell(
-                          onTap: () {},
-                          child: Image(
-                              width: 256,
-                              height: 256,
-                              fit: BoxFit.fill,
-                              image: AssetImage('images/uploadfile.png'))),
-                    if (imageFile != null) Image.file(File(imageFile!.path)),
-                    Text(
-                      scannedText,
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Color(0xff1738EB),
-                        fontWeight: FontWeight.w500,
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: InkWell(
+                            onTap: () {
+                              getImage(ImageSource.gallery);
+                            },
+                            child: Image(
+                                width: 200,
+                                height: 200,
+                                fit: BoxFit.fill,
+                                image: AssetImage('images/uploadfile.png'))),
+                      ),
+                    if (imageFile != null)
+                      Container(
+                          height: 400,
+                          width: 400,
+                          child: Image.file(File(imageFile!.path))),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(5, 10, 10, 5),
+                      color: Color.fromARGB(255, 231, 247, 255),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          scannedText,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 31, 31, 31),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
                     )
                   ],
@@ -92,6 +122,10 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
   //     setState(() {});
   //   }
   // }
+  void speakText(String text) async {
+    await flutterTts.speak(text);
+  }
+
   void getImage(ImageSource source) async {
     try {
       final pickedImage = await ImagePicker().pickImage(source: source);
@@ -115,10 +149,17 @@ class _RecognitionScreenState extends State<RecognitionScreen> {
     RecognizedText recognisedText = await textDetector.processImage(inputImage);
     await textDetector.close();
     scannedText = "";
+
     for (TextBlock block in recognisedText.blocks) {
       for (TextLine line in block.lines) {
-        scannedText = scannedText + line.text + "\n";
+        scannedText += line.text + ' ';
       }
+    }
+
+    sentences = scannedText.split('. ');
+
+    for (String sentence in sentences) {
+      scannedText += sentence + '.\n';
     }
     textScanning = false;
     setState(() {});
